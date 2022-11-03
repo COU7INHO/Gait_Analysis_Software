2f6bbd6cb87a085ed53dc40cf23d5b174f935421
COU7INHO <104756403+COU7INHO@users.noreply.github.com>
2022-11-03 16:55:41 +0000

    Update measure class file

--- measure/object_detector.py
+++ measure/object_detector.py
@@ -21,11 +21,6 @@ class HomogeneousBgDetector():
         for cnt in contours:
             area = cv2.contourArea(cnt)
             if area > 2000:
-                #cnt = cv2.approxPolyDP(cnt, 0.03*cv2.arcLength(cnt, True), True)
                 objects_contours.append(cnt)
 
         return objects_contours
-
-    # def get_objects_rect(self):
-    #     box = cv2.boxPoints(rect)  # cv2.boxPoints(rect) for OpenCV 3.x
-    #     box = np.int0(box)
