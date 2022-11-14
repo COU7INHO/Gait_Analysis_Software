ffa10473e92d2fc9e006989953532b85e92163d3
COU7INHO <104756403+COU7INHO@users.noreply.github.com>
2022-11-14 10:10:10 +0000

    class update

--- vision_calibration/CalibrationModel.py
+++ vision_calibration/CalibrationModel.py
@@ -5,6 +5,8 @@ from GetBothCamsImgs import GetCalibrationImages
 
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 CRITERIA_STEREO = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+FLAGS = 0
+FLAGS |= cv2.CALIB_FIX_INTRINSIC
 
 class CalibrationModel(GetCalibrationImages):
 
@@ -22,65 +24,61 @@ class CalibrationModel(GetCalibrationImages):
         objp = np.zeros((self.chessboardrows * self.chessboardcols, 3), np.float32)
         objp[:,:2] = np.mgrid[0:self.chessboardrows,0:self.chessboardcols].T.reshape(-1,2)
         objp = objp * self.squaresize
-        objpoints = [] 
-        imgpointsL = []
-        imgpointsR = [] 
+        self.objpoints = [] 
+        self.imgpointsL = []
+        self.imgpointsR = [] 
 
         imagesLeft = sorted(glob.glob(self.path1 + "*.png"))
         imagesRight = sorted(glob.glob(self.path2 + "*.png"))
         cv2.waitKey(2000)
+
         for imgLeft, imgRight in zip(imagesLeft, imagesRight):
 
-            imgL = cv2.imread(imgLeft)
-            imgR = cv2.imread(imgRight)
-            grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-            grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+            self.imgL = cv2.imread(imgLeft)
+            self.imgR = cv2.imread(imgRight)
+            self.grayL = cv2.cvtColor(self.imgL, cv2.COLOR_BGR2GRAY)
+            self.grayR = cv2.cvtColor(self.imgR, cv2.COLOR_BGR2GRAY)
 
-            retL, cornersL = cv2.findChessboardCorners(grayL, self.chessboardsize, None)
-            retR, cornersR = cv2.findChessboardCorners(grayR, self.chessboardsize, None)
+            self.retL, cornersL = cv2.findChessboardCorners(self.grayL, self.chessboardsize, None)
+            self.retR, cornersR = cv2.findChessboardCorners(self.grayR, self.chessboardsize, None)
 
-            if retL and retR == True:
+            if self.retL and self.retR == True:
 
-                objpoints.append(objp)
+                self.objpoints.append(objp)
 
-                cornersL = cv2.cornerSubPix(grayL, cornersL, (11,11), (-1,-1), CRITERIA)
-                imgpointsL.append(cornersL)
+                cornersL = cv2.cornerSubPix(self.grayL, cornersL, (11,11), (-1,-1), CRITERIA)
+                self.imgpointsL.append(cornersL)
 
-                cornersR = cv2.cornerSubPix(grayR, cornersR, (11,11), (-1,-1), CRITERIA)
-                imgpointsR.append(cornersR)
+                cornersR = cv2.cornerSubPix(self.grayR, cornersR, (11,11), (-1,-1), CRITERIA)
+                self.imgpointsR.append(cornersR)
 
-                cv2.drawChessboardCorners(imgL, self.chessboardsize, cornersL, retL)
-                cv2.drawChessboardCorners(imgR, self.chessboardsize, cornersR, retR)
-                imgRL = np.hstack((imgL, imgR))
+                cv2.drawChessboardCorners(self.imgL, self.chessboardsize, cornersL, self.retL)
+                cv2.drawChessboardCorners(self.imgR, self.chessboardsize, cornersR, self.retR)
+                imgRL = np.hstack((self.imgL, self.imgR))
                 cv2.imshow("Chessboard corners", imgRL)
                 cv2.waitKey(2000)
 
         cv2.destroyAllWindows()
 
-#* Calibration
-        retL, cameraMatrixL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objpoints, imgpointsL, self.framesize, None, None)
-        heightL, widthL, channelsL = imgL.shape
-        newCameraMatrixL, roi_L = cv2.getOptimalNewCameraMatrix(cameraMatrixL, distL, (widthL, heightL), 1, (widthL, heightL))
+    def calibration(self):
 
-        retR, cameraMatrixR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objpoints, imgpointsR, self.framesize, None, None)
-        heightR, widthR, channelsR = imgR.shape
-        newCameraMatrixR, roi_R = cv2.getOptimalNewCameraMatrix(cameraMatrixR, distR, (widthR, heightR), 1, (widthR, heightR))
+        self.retL, cameraMatrixL, self.distL, self.rvecsL, self.tvecsL = cv2.calibrateCamera(self.objpoints, self.imgpointsL, self.framesize, None, None)
+        heightL, widthL, self.channelsL = self.imgL.shape
+        self.newCameraMatrixL, self.roi_L = cv2.getOptimalNewCameraMatrix(cameraMatrixL, self.distL, (widthL, heightL), 1, (widthL, heightL))
 
-#* Stereo vision calibration
-        flags = 0
-        flags |= cv2.CALIB_FIX_INTRINSIC
+        self.retR, cameraMatrixR, self.distR, self.rvecsR, self.tvecsR = cv2.calibrateCamera(self.objpoints, self.imgpointsR, self.framesize, None, None)
+        heightR, widthR, self.channelsR = self.imgR.shape
+        self.newCameraMatrixR, self.roi_R = cv2.getOptimalNewCameraMatrix(cameraMatrixR, self.distR, (widthR, heightR), 1, (widthR, heightR))
+    
+    def matrixcalculation(self):
+        self.retStereo, self.newCameraMatrixL, self.distL, self.newCameraMatrixR, self.distR, self.rot, self.trans, self.essentialMatrix, self.fundamentalMatrix = cv2.stereoCalibrate(self.objpoints, self.imgpointsL, self.imgpointsR, self.newCameraMatrixL, self.distL, self.newCameraMatrixR, self.distR, self.grayL.shape[::-1], CRITERIA_STEREO, FLAGS)
+   
+    def newmatrix(self):
+        rectifyScale = 1
+        rectL, rectR, projMatrixL, projMatrixR, Q, self.roi_L, self.roi_R = cv2.stereoRectify(self.newCameraMatrixL, self.distL, self.newCameraMatrixR, self.distR, self.grayL.shape[::-1], self.rot, self.trans, rectifyScale,(0,0))
 
-        criteria_stereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-#* Transformation between the two cameras and calculate Essential and Fundamental matrix
-        retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv2.stereoCalibrate(objpoints, imgpointsL, imgpointsR, newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], criteria_stereo, flags)
-
-#* Stereo Rectification
-        rectifyScale= 1
-        rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv2.stereoRectify(newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], rot, trans, rectifyScale,(0,0))
-
-        stereoMapL = cv2.initUndistortRectifyMap(newCameraMatrixL, distL, rectL, projMatrixL, grayL.shape[::-1], cv2.CV_16SC2)
-        stereoMapR = cv2.initUndistortRectifyMap(newCameraMatrixR, distR, rectR, projMatrixR, grayR.shape[::-1], cv2.CV_16SC2)
+        stereoMapL = cv2.initUndistortRectifyMap(self.newCameraMatrixL, self.distL, rectL, projMatrixL, self.grayL.shape[::-1], cv2.CV_16SC2)
+        stereoMapR = cv2.initUndistortRectifyMap(self.newCameraMatrixR, self.distR, rectR, projMatrixR, self.grayR.shape[::-1], cv2.CV_16SC2)
 
         cv_file = cv2.FileStorage("./vision_calibration/stereoMap.xml", cv2.FILE_STORAGE_WRITE)
 
