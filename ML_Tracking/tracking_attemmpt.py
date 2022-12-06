d2956fbcdbd4f0d3fdd44d86e21f9c04bda64e62
COU7INHO <104756403+COU7INHO@users.noreply.github.com>
2022-12-06 08:38:45 +0000

    new file to track, not finished

--- ML_Tracking/tracking_attemmpt.py
+++ ML_Tracking/tracking_attemmpt.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import imutils
 from time import time
+import math
 
 
 # Load Yolo
@@ -17,6 +18,9 @@ loop_time = time()
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
 
+center_points_prev_frame = []
+tracking_objects = {}
+track_id = 0
 
 while camera.isOpened():
 
@@ -26,6 +30,8 @@ while camera.isOpened():
     fps = 1/(time() - loop_time)
     loop_time = time()
     
+    center_points_cur_frame = []
+
     # Loading image
     height, width, channels = frame.shape
 
@@ -33,13 +39,14 @@ while camera.isOpened():
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
 
     net.setInput(blob)
-    outs = net.forward(output_layers)
+    outputs = net.forward(output_layers)
 
     # Showing informations on the screen
     class_ids = []
     confidences = []
     boxes = []
-    for out in outs:
+
+    for out in outputs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
@@ -52,14 +59,25 @@ while camera.isOpened():
                 x2 = int(x + w/2)
                 y2 = int(y + h/2)
                 
-                # Draw a circle in the center of each bounding box
-                cv2.circle(frame, (int(x), int(y)), 5, (255, 50, 10), -1 )
-
+                center_points_cur_frame.append((int(x), int(y)))
                 boxes.append([x1, y1, x2, y2])
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-                
+
+        for point in center_points_cur_frame:
+            for point2 in center_points_prev_frame:
+                distance = math.hypot(point2[0] - point[0], point2[1 ] - point[1])
+
+                if distance < 10:
+                    tracking_objects[track_id] = point
+                    track_id += 1
+
+        for object_id, point in tracking_objects.items():
+            cv2.circle(frame, point, 5, (255, 50, 10), -1 )
+            cv2.putText(frame, str(object_id), (point[0], point[1] - 40), 1, cv2.FONT_HERSHEY_COMPLEX, (12, 255, 0), 2)
+        
+
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
     for i in range(len(boxes)):
         if i in indexes:
@@ -69,11 +87,15 @@ while camera.isOpened():
 
             #?cv2.putText(frame, label, (x, y - 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
 
-
     cv2.putText(frame, f"FPS: {str(round(fps, 3))}", (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 3)
     cv2.imshow("Detecting markers...", frame)
-    
-    key = cv2.waitKey(1)
+
+    center_points_prev_frame = center_points_cur_frame.copy()
+    print("Center points current frame: ", center_points_cur_frame)
+    print("Previous points current frame: ", center_points_prev_frame)
+
+
+    key = cv2.waitKey(0)
 
     if key == ord('q'):
         break
