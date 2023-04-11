c956cfacab4024ddd8a13ab470f847014660ba1e
COU7INHO <104756403+COU7INHO@users.noreply.github.com>
2023-04-11 16:24:59 +0100

    update GUI

--- model_v2/main_gui.py
+++ model_v2/main_gui.py
@@ -13,12 +13,10 @@ import cv2
 from motionAnalysis_class import MotionAnalysis
 
 
-
 class VideoData(QLabel):
     def __init__(self):
         super(VideoData, self).__init__()
-        '''self.video = cv2.VideoCapture('/Users/tiagocoutinho/Desktop/videos/espelho.mov')
-        '''
+
         self.init_video = MotionAnalysis("/Users/tiagocoutinho/Desktop/videos/espelho.mov", "Motion Analysis")
 
         self.init_video.openCamera()
@@ -34,7 +32,6 @@ class VideoData(QLabel):
 
         self.knee_angle = 0
         
-
     def update_frame(self):
 
         self.init_video.getFrame()
@@ -53,11 +50,21 @@ class VideoData(QLabel):
         q_image = QImage(self.video_frame.data, width, height, channels * width, QImage.Format_RGB888)
         self.setPixmap(QPixmap.fromImage(q_image))
         
-    def getAngles(self):
-        for knee_ang in self.init_video.ankle_angles:
-            self.knee_angle = knee_ang
-        return self.knee_angle
+    def getAngle(self, joint):
+        angles = None
+        if joint == "Hip":
+            angles = self.init_video.hip_angles
+        elif joint == "Knee":
+            angles = self.init_video.knee_angles
+        elif joint == "Ankle":
+            angles = self.init_video.ankle_angles
+        else:
+            raise ValueError("Invalid angle")
 
+        for angle in angles:
+             final_angle = angle
+        return final_angle
+    
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -67,74 +74,105 @@ class MainWindow(QWidget):
 
         self.x_history = []
         self.y_history = []
+        self.x_history2 = []
+        self.y_history2 = []
+        self.x_history3 = []
+        self.y_history3 = []
 
     def init_ui(self):
-        self.setWindowTitle("Webcam Viewer")
+        self.setWindowTitle("Gait analysis")
         self.setGeometry(100, 100, 1600, 480)
 
-        # Create layout for the main window
         main_layout = QHBoxLayout()
 
-        # Create frame1 for the plot
         plot_frame = QFrame()
         plot_frame.setFrameShape(QFrame.StyledPanel)
 
-        # Create a Figure object for the plot
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
 
-        # Add the canvas to frame1
         v_layout = QVBoxLayout()
         v_layout.addWidget(self.canvas)
         plot_frame.setLayout(v_layout)
 
-        self.ax = self.figure.add_subplot(111)
-        self.ax.set_xlabel("Frame")
+        self.ax = self.figure.add_subplot(311)
         self.ax.set_ylabel("Angle (degrees)")
-        self.ax.set_title("Angle plot")
+        self.ax.set_title("Hip angle")
         self.line, = self.ax.plot([], [])
+        self.ax.set_xticklabels([]) 
+
+        self.ax2 = self.figure.add_subplot(312)
+        self.ax2.set_ylabel("Angle (degrees)")
+        self.ax2.set_title("Knee angle")
+        self.line2, = self.ax2.plot([], [])
+        self.ax2.set_xticklabels([]) 
+
+        self.ax3 = self.figure.add_subplot(313)
+        self.ax3.set_xlabel("Frame")
+        self.ax3.set_ylabel("Angle (degrees)")
+        self.ax3.set_title("Hip angle")
+        self.line3, = self.ax3.plot([], [])
 
         main_layout.addWidget(plot_frame)
 
-        # Create frame2 for the webcam widget
         webcam_frame = QFrame()
         webcam_frame.setFrameShape(QFrame.StyledPanel)
 
-        # Create a QVBoxLayout for the webcam frame
         webcam_layout = QVBoxLayout()
 
-        # Create the webcam widget and add it to the layout
         webcam_layout.addWidget(self.webcam_widget)
-
         webcam_frame.setLayout(webcam_layout)
-
         main_layout.addWidget(webcam_frame)
 
         self.setLayout(main_layout)
 
-        # Call function to update the plot with real-time data
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update_plot)
+        self.timer.timeout.connect(lambda: self.update_plot("Hip"))
         self.timer.start(1)
 
+        self.timer2 = QTimer()
+        self.timer2.timeout.connect(lambda: self.update_plot("Knee"))
+        self.timer2.start(1)
 
-    def update_plot(self):
+        self.timer3 = QTimer()
+        self.timer3.timeout.connect(lambda: self.update_plot("Ankle"))
+        self.timer3.start(1)
+        
+    def update_plot(self, angle:str):
+            
+        if angle == "Hip":
+            x = self.webcam_widget.current_frame
+            y = self.webcam_widget.getAngle("Hip")
+            self.x_history.append(x)
+            self.y_history.append(y)
+            self.line.set_data(self.x_history, self.y_history)
+            self.ax.relim()
+            self.ax.autoscale_view()
+            self.ax.set_ylim([-20, 30])
+            self.canvas.draw()
 
-        x = self.webcam_widget.current_frame
-        y = self.webcam_widget.getAngles()
- 
-        self.x_history.append(x)
-        self.y_history.append(y)
+        elif angle == "Knee":
+            x = self.webcam_widget.current_frame
+            y = self.webcam_widget.getAngle("Knee")
+            self.x_history2.append(x)  
+            self.y_history2.append(y)  
+            self.line2.set_data(self.x_history2, self.y_history2) 
+            self.ax2.relim() 
+            self.ax2.autoscale_view()  
+            self.ax2.set_ylim([-5, 70])
+            self.canvas.draw()
 
-        self.line.set_data(self.x_history, self.y_history)
-        self.ax.relim()
-        self.line.set_color('blue')  
-        self.line.set_linestyle('-') 
-        self.ax.set_xlim([0, max(self.x_history)])  # Set the x-axis limit from zero to the maximum value in x_history
-        #self.ax.set_ylim([-20, 30])  # Replace y_min and y_max with your desired limits
-        self.ax.autoscale_view()
-        self.canvas.draw()
-
+        elif angle == "Ankle":
+            x = self.webcam_widget.current_frame
+            y = self.webcam_widget.getAngle("Ankle")
+            self.x_history3.append(x)  
+            self.y_history3.append(y)  
+            self.line3.set_data(self.x_history3, self.y_history3) 
+            self.ax3.relim() 
+            self.ax3.autoscale_view()  
+            self.ax3.set_ylim([-20, 25])
+            self.canvas.draw()
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
