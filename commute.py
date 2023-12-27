51f977694c06ee508384bed6eea8e53249b84389
COU7INHO <104756403+COU7INHO@users.noreply.github.com>
2023-12-27 09:00:50 +0000

    delete files

--- commute.py
+++
@@ -1,7 +0,0 @@
-travel_distance = int(input("How long is your commute?\n>> "))
-
-avg_speed = int(input("What will be the average speed?\n>> "))
-
-travel_time = travel_distance / avg_speed
-
-print(round(travel_time * 60, 1))
